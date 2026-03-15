2876ecd6994cb6f7d3fcd097a15a275734595d32
Umersaleem98 <umer.saleem.abbasi109@gmail.com>
2026-03-15 15:15:57 -0700

    template integrate properky and home setting done

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+@include('layouts.templates.head')
+
+<body>
+
+    <!-- Preloader -->
+    <div class="preloader">
+        <div class="preloader-inner">
+            <div class="preloader-icon">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <!-- /End Preloader -->
+
+   @include('layouts.templates.header')
+   @include('layouts.templates.hero')
+   @include('layouts.templates.content')
+
+
+
+
+  @include('layouts.templates.footer')
+
+    <!-- ========================= scroll-top ========================= -->
+    <a href="#" class="scroll-top">
+        <i class="lni lni-chevron-up"></i>
+    </a>
+@include('layouts.templates.script')
---
+++ resources/views/layouts/templates/head.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>IRA-Tech.</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('templates/assets/images/favicon.svg') }}" />
+
+    <!-- ========================= CSS here ========================= -->
+    <link rel="stylesheet" href="{{ asset('templates/assets/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('templates/assets/css/LineIcons.3.0.css') }}" />
+    <link rel="stylesheet" href="{{ asset('templates/assets/css/tiny-slider.css') }}" />
+    <link rel="stylesheet" href="{{ asset('templates/assets/css/glightbox.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('templates/assets/css/main.css') }}" />
+
+</head>
